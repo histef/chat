26531ecdf46429b0d5c59866693680794f2ea30e
Stefanie Chin <hi.stef.c@gmail.com>
2018-12-28 14:55:30 -0800

    chore & refactor, add some more documentation. for better understanding

--- chat/server.js
+++ chat/server.js
@@ -2,28 +2,29 @@ const app = require('express')(); //initialize 'app' to be a fn hndler to supply
 // const express = require('express');
 // const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+const io = require('socket.io')(server);
 
 users = [];
 connections = [];
 
+//static file
 app.get('/', (req,res)=> {
   res.sendFile(__dirname + '/index.html')
 })
 
-// listen on the 'connection' event for incoming sockets, and logs it to the console.
-io.on('connection', function(socket){
+// listens on the 'connection' event for incoming sockets(new user), and logs it to the console.
+io.on('connection', function(socket){ //connects socket.io on server, now need to connect to client(see index.html)
   connections.push(socket);
   io.emit('this', { will: 'be received by everyone'});
-  console.log('a user connected', connections.length);
+  console.log('new user connected', socket.id, connections.length);
 
   socket.on('private message', function (from, msg) {
     console.log('I received a private message by ', from, ' saying ', msg);
   });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function (data) {
     connections.splice(connections.indexOf(socket), 1)
-    // io.emit('user disconnected', connections.length);
+    io.emit('user disconnected', connections.length);
     console.log('user disconnected', connections.length)
   });
 
@@ -34,6 +35,5 @@ io.on('connection', function(socket){
 });
 
 const port = process.env.PORT || 3000;
-server.listen(port);
-console.log('server running');
+server.listen(port, console.log('server running'));
 
