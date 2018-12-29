57bf5a76dd33f2d4ca0c8d9d884d56a4ff50a7c1
Stefanie Chin <hi.stef.c@gmail.com>
2018-12-29 13:51:03 -0800

    doc and clean up code, clear message field

--- chat/public/chat.js
+++ chat/public/chat.js
@@ -1,33 +1,34 @@
 const socket = io.connect('http://localhost:3000');
 
+const username = document.querySelector('#username');
 const message = document.getElementById("message");
 const sendBtn = document.querySelector('#send');
 const output = document.querySelector('#output');
 //for broadcasting a message
 const broadcast = document.querySelector('#broadcast');
-const username = document.querySelector('#username');
-// const chat = document.querySelector('#chat');
-// const messageForm = document.querySelector("#message-form");
 
-// emit events
+
+/*EMIT EVENTS (CLIENT)*/
 //will emit to everyone including sender
-sendBtn.addEventListener('click', (e) => {
+//note: socket.emit takes in the actual values
+sendBtn.addEventListener('click', () => {
   socket.emit('chat-message', {
     username: username.value,
     message: message.value
   })
 })
 
-//emit to everyone BUT sender. 1st param must match server-side socket.on
+//emit to everyone BUT sender.
 message.addEventListener('keypress', () => {
-  socket.emit('typing', message.value)
+  socket.emit('typing', username.value)
 })
 
-//listen for events
+//listen for events. 1st param must match server-side socket.on
 socket.on('chat-message', data => {
   //clear broadcast when user sends message
   broadcast.innerHTML = '';
   output.innerHTML += `<p><strong>${data.username}: </strong> ${data.message}</p>`;
+  message.value = '';
 })
 
 socket.on('typing', data => {
